07b1239a6a99d74d025d039138100e583086a7af
Barret J. Nobel <bear.nobel@gmail.com>
2017-12-01 14:26:08 -0500

    updated program so that you can pass in grades to the query and print out the ave grade and letter grade

--- gradeBook/program.js
+++ gradeBook/program.js
@@ -2,5 +2,22 @@ var book = require("./lib/grades").book;
 var express = require("express");
 var app = express();
 
+app.get("/", function(req, res){
+    res.send("First Response!");
+});
+
+app.get("/grade", function(req, res){
+    var grades = req.query.grades.split(",");
+
+    for( var i = 0; i < grades.length; i += 1){
+        book.addGrade( parseInt(grades[i]) );
+    }
+
+    var ave = book.getAverage();
+    var letter = book.getLetterGrade();
+
+    res.send( "Your average grade is: " + ave + "\nGrade: " + letter );
+});
+
 app.listen( 3000 );
 console.log( "server ready ..." );
